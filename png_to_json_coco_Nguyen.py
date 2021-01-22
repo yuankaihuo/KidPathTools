58af1ae09c6e016533bb2be5a163cd2274b29177
EthanHNguyen <B_j46jGM7D9ECUrn>
2021-01-21 18:56:31 -0600

    successfully converted to COCO

--- png_to_json_coco_Nguyen.py
+++ png_to_json_coco_Nguyen.py
@@ -5,8 +5,142 @@ import re
 import fnmatch
 from PIL import Image
 import numpy as np
-import glob
 from pycococreatortools import pycococreatortools
-from skimage.measure import label
 
-if __name__ = "__main__":
+
+INPUT_ROOT_DIR = '/home/sybbure/CircleNet/MoNuSeg-Test/coco-mask'
+
+ROOT_DIR = '/home/sybbure/CircleNet/MoNuSeg-Test/MoNuSeg-coco'
+
+INFO = {
+    "description": "MICCAI 2018 - MoNuSeg",
+    "url": "https://monuseg.grand-challenge.org/Home/",
+    "version": "0.1.0",
+    "year": 2018,
+    "contributor": "EthanHNguyen",
+    "date_created": datetime.datetime.utcnow().isoformat(' ')
+}
+
+LICENSES = [
+    {
+        "id": 1,
+        "name": "Attribution-NonCommercial-ShareAlike License",
+        "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"
+    }
+]
+
+CATEGORIES = [
+    {
+        'id': 1,
+        'name': 'nuclei',
+        'supercategory': 'nuclei',
+    }
+]
+
+
+def filter_for_jpeg(root, files):
+    file_types = ['*.jpeg', '*.jpg']
+    file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
+    files = [os.path.join(root, f) for f in files]
+    files = [f for f in files if re.match(file_types, f)]
+
+    return files
+
+
+def filter_for_annotations(root, files, image_filename):
+    file_types = ['*.png']
+    file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
+    basename_no_extension = os.path.splitext(os.path.basename(image_filename))[0]
+    file_name_prefix = basename_no_extension + '.*'
+    files = [os.path.join(root, f) for f in files]
+    files = [f for f in files if re.match(file_types, f)]
+    files = [f for f in files if re.match(file_name_prefix, os.path.splitext(os.path.basename(f))[0])]
+
+    return files
+
+def walklevel(some_dir, level=1):
+    some_dir = some_dir.rstrip(os.path.sep)
+    assert os.path.isdir(some_dir)
+
+    num_sep = some_dir.count(os.path.sep)
+
+    for root, dirs, files in os.walk(some_dir):
+        yield root, dirs, files
+        num_sep_this = root.count(os.path.sep)
+        if num_sep + level <= num_sep_this:
+            del dirs[:]
+
+def main():
+    coco_output = {
+        "info": INFO,
+        "licenses": LICENSES,
+        "categories": CATEGORIES,
+        "images": [],
+        "annotations": []
+    }
+
+    image_id = 1
+    segmentation_id = 1
+
+    # filter for jpeg images
+    for root, folders, files in walklevel(INPUT_ROOT_DIR, 0):
+        # image_files = filter_for_jpeg(root, files)
+
+        # go through each image
+        for image_folder in folders:
+            # print(image_folder)
+            folder_path = os.path.join(INPUT_ROOT_DIR, image_folder)
+            image_path = os.path.join(folder_path, 'image', 'image.png')
+
+            image = Image.open(image_path)
+            image_info = pycococreatortools.create_image_info(
+                image_id, os.path.basename(image_folder), image.size)
+
+            print(image_info)
+
+            coco_output["images"].append(image_info)
+
+            INPUT_MASK_DIR = os.path.join(folder_path, 'masks')
+
+            # filter for associated png annotations
+            for root, _, files in os.walk(INPUT_MASK_DIR):
+                annotation_files = files
+
+                # go through each associated annotation
+                for annotation_filename in annotation_files:
+                    annotation_path = os.path.join(folder_path, 'masks', annotation_filename)
+                    IMAGE_DIR = os.path.join(ROOT_DIR, 'train')
+
+                    # Create directories if they don't exist
+                    if not os.path.exists(IMAGE_DIR):
+                        os.makedirs(IMAGE_DIR)
+
+                    image_filename_new = os.path.join(ROOT_DIR, 'train', image_folder + '.png')
+                    os.system("cp '%s' '%s'" % (image_path, image_filename_new))
+
+                    # class_id = [x['id'] for x in CATEGORIES if x['name'] in new_annotation_name][0]
+                    class_id = 1
+
+                    category_info = {'id': class_id, 'is_crowd': 'crowd' in image_folder}
+                    binary_mask = np.asarray(Image.open(annotation_path)
+                                             .convert('1')).astype(np.uint8)
+
+                    annotation_info = pycococreatortools.create_annotation_info(
+                        segmentation_id, image_id, category_info, binary_mask,
+                        image.size, tolerance=2)
+
+                    # print(annotation_info)
+
+                    if annotation_info is not None:
+                        coco_output["annotations"].append(annotation_info)
+
+                    segmentation_id = segmentation_id + 1
+
+            image_id = image_id + 1
+
+    with open(os.path.join(ROOT_DIR, 'MoNuSeg2018-train.json').format(ROOT_DIR), 'w') as output_json_file:
+        json.dump(coco_output, output_json_file)
+
+
+if __name__ == "__main__":
+    main()
--- png_to_mask_Nguyen.py
+++ png_to_mask_Nguyen.py
@@ -19,58 +19,62 @@ def png_to_big_mask(png_file, xml_file, output_dir):
         Masklayer = layers[0]
         contours = Masklayer['Regions']['Region']
 
-    # Create output directories
-    output_img_dir = os.path.join(output_dir, 'train_whole')
-    output_cimg_dir = os.path.join(output_dir, 'QA_whole')
-    output_mask_dir = os.path.join(output_dir, 'labels_whole')
+    # Create a unique directory for each image
+    output_img_dir = os.path.join(output_dir, file.split(".")[0])
 
     if not os.path.exists(output_img_dir):
         os.makedirs(output_img_dir)
-    if not os.path.exists(output_cimg_dir):
-        os.makedirs(output_cimg_dir)
-    if not os.path.exists(output_mask_dir):
-        os.makedirs(output_mask_dir)
 
-    # Create the contour and mask containers
+    img_dir = os.path.join(output_img_dir, "image")
+    mask_dir = os.path.join(output_img_dir, "masks")
+
+    if not os.path.exists(img_dir):
+        os.makedirs(img_dir)
+
+    if not os.path.exists(mask_dir):
+        os.makedirs(mask_dir)
+
+    # Open the image
     img = Image.open(png_file)
     img = np.array(img)  # read in image as NumPy array
-    mask = np.zeros(img.shape, dtype=np.uint16)
 
-    # # For each region of interest, create a uniquely valued mask
-    # for ci in range(len(contours)):
-    #     contour = contours[ci]
-    #     vertices = contour["Vertices"]["Vertex"]
-    #
-    #     # Read data into array
-    #     # For each vertex in the ROI
-    #     cnt = np.zeros((len(vertices), 1, 2))
-    #     for vi in range(len(vertices)):
-    #         # Read in x and y co-ordinate of each vertex
-    #         cnt[vi, 0, 0] = float(vertices[vi]['@X'])
-    #         cnt[vi, 0, 1] = float(vertices[vi]['@Y'])
-    #
-    #     # Draw contours (note - color is green)
-    #     cv2.drawContours(cimg, [cnt.astype(int)], -1, (0, 255, 0), 1)
-    #
-    #     # Draw masks (note - color is white and thickness = -1)
-    #     cv2.drawContours(mask, [cnt.astype(int)], -1, (255, 255, 255), -1)
+    # For each region of interest, create a uniquely valued mask and save
+    for ci in range(len(contours)):
+        mask = np.zeros(img.shape, dtype=np.uint8)
+        contour = contours[ci]
+        vertices = contour["Vertices"]["Vertex"]
 
-    tiff_name = "%s-wholeimage.tiff" % file.split(".")[0]
+        # Read data into array
+        # For each vertex in the ROI
+        cnt = np.zeros((len(vertices), 1, 2))
+        for vi in range(len(vertices)):
+            # Read in x and y co-ordinate of each vertex
+            cnt[vi, 0, 0] = float(vertices[vi]['@X'])
+            cnt[vi, 0, 1] = float(vertices[vi]['@Y'])
 
-    img_out_file = os.path.join(output_img_dir, tiff_name)
-    cimg_out_file = os.path.join(output_cimg_dir, tiff_name)
-    mask_out_file = os.path.join(output_mask_dir, tiff_name)
+        # Draw contours (note - color is green)
+        # cv2.drawContours(cimg, [cnt.astype(int)], -1, (0, 255, 0), 1)
+
+        # Draw masks (note - color is white and thickness = -1)
+        cv2.drawContours(mask, [cnt.astype(int)], -1, (255, 255, 255), -1)
+
+        mask_name = "mask_%s.png" % ci
+        # cimg_out_file = os.path.join(output_cimg_dir, )
+        mask_out_file = os.path.join(mask_dir, mask_name)
+
+        # cimg_out = Image.fromarray(np.concatenate((img, cimg, mask), axis=1), 'I;16')
+        # cimg_out.save(cimg_out_file)
+
+        mask_out = Image.fromarray(mask)
+        mask_out.save(mask_out_file)
+
+    # Save the image
+    img_out_file = os.path.join(img_dir, "image.png")
 
     img_out = Image.fromarray(img)
     img_out.save(img_out_file)
 
-    # cimg_out = Image.fromarray(np.concatenate((img, cimg, mask), axis=1), 'I;16')
-    # cimg_out.save(cimg_out_file)
-
-    mask_out = Image.fromarray(mask, 'I;16')
-    mask_out.save(mask_out_file)
-
-    return img_out_file, mask_out_file
+    return img_dir, mask_dir
 
 def save_cropped_img_mask(big_img_file, big_mask_file, output_dir, create_number_per_image, fname):
     # Create folders for output
