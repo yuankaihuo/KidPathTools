6abc57ede394119e2daacf779f2ab49737e53e7f
EthanHNguyen <B_j46jGM7D9ECUrn>
2021-01-30 12:47:10 -0600

    Split dataset into training, validation, testing cohort

--- png_to_json_coco_Nguyen.py
+++ png_to_json_coco_Nguyen.py
@@ -8,9 +8,9 @@ import numpy as np
 from pycococreatortools import pycococreatortools
 
 
-INPUT_ROOT_DIR = '/home/sybbure/CircleNet/MoNuSeg-Test/coco-mask'
+INPUT_ROOT_DIR = '/home/sybbure/CircleNet/MoNuSeg-Test/png-instance-segmentation/roi'
 
-ROOT_DIR = '/home/sybbure/CircleNet/MoNuSeg-Test/MoNuSeg-coco'
+ROOT_DIR = '/home/sybbure/CircleNet/MoNuSeg-Test/png-coco-like'
 
 INFO = {
     "description": "MICCAI 2018 - MoNuSeg",
@@ -58,6 +58,13 @@ def filter_for_annotations(root, files, image_filename):
 
     return files
 
+def filter_for_xml(files):
+    file_types = ['*.xml']
+    file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
+    files = [f for f in files if re.match(file_types, f)]
+
+    return files
+
 def walklevel(some_dir, level=1):
     some_dir = some_dir.rstrip(os.path.sep)
     assert os.path.isdir(some_dir)
@@ -81,6 +88,27 @@ def main():
 
     image_id = 1
     segmentation_id = 1
+
+    sublist = {}
+    sublist['train'] = ['TCGA-HE-7129-01Z-00-DX1', 'TCGA-A7-A13F-01Z-00-DX1', 'TCGA-KB-A93J-01A-01-TS1', 'TCGA-G9-6356-01Z-00-DX1', 'TCGA-B0-5711-01Z-00-DX1', 'TCGA-G9-6336-01Z-00-DX1', 'TCGA-CH-5767-01Z-00-DX1', 'TCGA-A7-A13E-01Z-00-DX1', 'TCGA-AR-A1AS-01Z-00-DX1', 'TCGA-G2-A2EK-01A-02-TSB', 'TCGA-G9-6348-01Z-00-DX1', 'TCGA-RD-A8N9-01A-01-TS1', 'TCGA-50-5931-01Z-00-DX1', 'TCGA-NH-A8F7-01A-01-TS1', 'TCGA-B0-5698-01Z-00-DX1', 'TCGA-HE-7128-01Z-00-DX1', 'TCGA-B0-5710-01Z-00-DX1', 'TCGA-AR-A1AK-01Z-00-DX1', 'TCGA-21-5784-01Z-00-DX1', 'TCGA-HE-7130-01Z-00-DX1']
+    sublist['validation'] = ['TCGA-18-5592-01Z-00-DX1', 'TCGA-38-6178-01Z-00-DX1', 'TCGA-21-5786-01Z-00-DX1', 'TCGA-G9-6363-01Z-00-DX1', 'TCGA-DK-A2I6-01A-01-TS1', 'TCGA-E2-A1B5-01Z-00-DX1', 'TCGA-G9-6362-01Z-00-DX1', 'TCGA-AY-A8YK-01A-01-TS1', 'TCGA-49-4488-01Z-00-DX1', 'TCGA-E2-A14V-01Z-00-DX1']
+    sublist['test'] = ['TCGA-AC-A2FO-01A-01-TS1', 'TCGA-ZF-A9R5-01A-01-TS1', 'TCGA-EJ-A46H-01A-03-TSC', 'TCGA-CU-A0YN-01A-02-BSB', 'TCGA-HC-7209-01A-01-TS1', 'TCGA-FG-A4MU-01B-01-TS1', 'TCGA-A6-6782-01A-01-BS1', 'TCGA-HT-8564-01Z-00-DX1', 'TCGA-44-2665-01B-06-BS6', 'TCGA-IZ-8196-01A-01-BS1', 'TCGA-2Z-A9J9-01A-01-TS1', 'TCGA-69-7764-01A-01-TS1', 'TCGA-AO-A0J2-01A-01-BSA', 'TCGA-GL-6846-01A-01-BS1']
+    types = ['train','validation','test']
+
+    # # Get files names for split
+    # for root, folders, files in walklevel('/home/sybbure/CircleNet/MoNuSegTestData', 0):
+    #     files = filter_for_xml(files)
+    #     print(files)
+    #     print(len(files))
+    #     idx = 0
+    #     for file in files:
+    #         files[idx] = file.split('.')[0]
+    #         idx += 1
+    #     print(files[:20])
+    #     print(len(files[:20]))
+    #
+    #     print(files[20:])
+    #     print(len(files[20:]))
 
     # filter for jpeg images
     for root, folders, files in walklevel(INPUT_ROOT_DIR, 0):
